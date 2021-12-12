0eaf4a18a3abb77204ad0d4bd1b341638ef51dc6
unillr <55926286+unillr@users.noreply.github.com>
2021-12-12 21:22:33 +0900

    /map 大幅な書き直し やり直しに上限を追加

--- commands/map.js
+++ commands/map.js
@@ -5,38 +5,27 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('map')
         .setDescription('VALORANTのMAPをランダムに選ぶよ！'),
-    async execute(interaction) {
-        const mapNames = ['Ascent', 'Bind', 'Breeze', 'Fracture', 'Haven', 'Icebox', 'Split'];
-        const pickedMapName = mapNames[Math.floor(Math.random() * mapNames.length)];
-
-        const attachment = new MessageAttachment()
-            .setFile(`./images/maps/${pickedMapName}.png`, `${pickedMapName}.png`);
-
-        const embed = new MessageEmbed()
-            .setTitle(pickedMapName.toUpperCase())
-            .setImage(`attachment://${pickedMapName}.png`)
-            .setColor('#fa4454');
-
-        const row = new MessageActionRow()
+    maps: ['Ascent', 'Bind', 'Breeze', 'Fracture', 'Haven', 'Icebox', 'Split'],
+    createReplyOptions: mapName => ({
+        files: [new MessageAttachment()
+            .setFile(`./images/maps/${mapName}.png`, `${mapName}.png`)],
+        embeds: [new MessageEmbed()
+            .setTitle(mapName.toUpperCase())
+            .setImage(`attachment://${mapName}.png`)
+            .setColor('#fa4454')],
+        components: [new MessageActionRow()
             .addComponents(
                 new MessageButton()
                     .setCustomId('repick')
                     .setLabel('選び直す')
                     .setStyle('PRIMARY'),
-            );
+            )],
+    }),
 
-        if (interaction.customId === 'repick') {
-            await interaction.update({
-                files: [attachment],
-                embeds: [embed],
-                components: [row],
-            });
-        } else {
-            await interaction.reply({
-                files: [attachment],
-                embeds: [embed],
-                components: [row],
-            });
-        }
+    async execute(interaction) {
+        const pickedMap = this.maps[Math.floor(Math.random() * this.maps.length)];
+        const options = this.createReplyOptions(pickedMap);
+
+        await interaction.reply(options);
     },
 };
--- events/repick.js
+++ events/repick.js
@@ -6,8 +6,18 @@ module.exports = {
         if (!interaction.isButton()) return;
         if (interaction.customId !== 'repick') return;
 
+        const filteredMaps = map.maps
+            .filter(mapName => mapName.toUpperCase() !== interaction.message.embeds[0].title);
+
+        const pickedMap = filteredMaps[Math.floor(Math.random() * filteredMaps.length)];
+        const options = map.createReplyOptions(pickedMap);
+
+        if (interaction.message.attachments.size > 4) {
+            options.components[0].components[0].setDisabled(true);
+        }
+
         try {
-            await map.execute(interaction);
+            await interaction.update(options);
         } catch (error) {
             console.error(error);
             await interaction.reply({ content: 'エラーがおこったよ!', ephemeral: true });
